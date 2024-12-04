e92981ec3a676d94708a4d9b1962b34fc4cf79b4
tinxte <150742495+Tinxte@users.noreply.github.com>
2024-12-04 11:14:57 -0600

    Initialization of project, amplify and vue front end setup

---
+++ vue-amplify/src/components/Container.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="container">
+        <transition name="f" appear>
+            <form @submit.prevent="Create()">
+                <label>New Workout</label>
+                <input 
+                type="text"
+                v-model="msg"
+                autocomplete="off"
+                >
+                <button>Add workout</button>
+            </form>
+            </transition>
+            <ExerciseList
+            :ExerciseLists="ExerciseLists"
+            :Delete="Delete" />
+            <transition name="m" appear>
+                <w-notification
+                v-show="showNotification"
+                warning
+                bottom="true"
+                round
+                shadow
+                transition="bounce"
+                >
+                Please enter a value.
+            </w-notification>
+            </transition>
+    </div>
+</template>
+
+<script>
+import ExerciseList from './ExerciseList.vue';
+
+export default {
+    components: {
+        ExerciseList,
+    },
+    data: () => ({
+        msg: "",
+        showNotification: false,
+        timeout: 2000,
+        exampleData: [
+            { id: 1, name: 'Item 1' },
+            { id: 2, name: 'Item 2' },
+            { id: 3, name: 'Item 3' },
+        ]
+    }),
+    methods: {
+        Create() {
+            setTimeout(() =>
+        { this.showNotification = false; }, this.timeout
+    );
+            if (this.msg.length) {
+                let id = Math.floor(Math.random()*100);
+                const next = { id, name: this.msg };
+                this.ExerciseList.push(next);
+            }
+        },
+        //TODO Define Delete method 6:12
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-amplify/src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import WaveUI from 'wave-ui'
+import 'wave-ui/dist/wave-ui.css'
+
+const app = createApp(App)
+
+app.use(WaveUI, { /* Some Wave UI options */ })
+// Or in Wave UI 2.x:
+// new WaveUI(app, { /* Some Wave UI options */ })
+
+app.mount('#app')
